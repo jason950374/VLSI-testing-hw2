5771b618e8bc1dc245ab7f1b9e1bd94346c9ff02
楊承翰 <jasonwanttoplay@gmail.com>
2016-12-12 17:26:34 +0800

    complete the main fix the bug in intializeOutTap implement the operation= of Matrix

--- Project1/Matrix.h
+++ Project1/Matrix.h
@@ -23,6 +23,7 @@ public:
 	~Matrix();
 	Matrix operator*(const Matrix& m2);
 	Matrix operator+(const Matrix& m2);
+	Matrix& operator=(const Matrix& m2);
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -45,9 +46,11 @@ template <class T> Matrix<T>::Matrix(T ** elements, int sizeRow, int sizeCLM) {
 
 template<class T> Matrix<T>::Matrix(const Matrix & M)
 {
-	elements = T[sizeRow][sizeCLM];
 	sizeRow = M.sizeRow;
 	sizeCLM = M.sizeCLM;
+	elements = new T *[sizeRow];
+	for (int row = 0; row < sizeRow; row++)
+		elements[row] = new T[sizeCLM];
 	for (int row = 0; row < sizeRow; row++) {
 		for (int clm = 0; clm < sizeCLM; clm++) {
 			elements[row][clm] = M.elements[row][clm];
@@ -62,16 +65,16 @@ template<class T> Matrix<T>::~Matrix()
 
 template<class T> Matrix<T> Matrix<T>::operator*(const Matrix & m2)
 {
-	if (sizeCLM != m2.sizeRow) {
-		cerr << "matrix size mismatch" << endl; return;
-	}
 	Matrix<T> newM = Matrix<T>(sizeRow, m2.sizeCLM);
+	if (sizeCLM != m2.sizeRow) {
+		cerr << "matrix size mismatch" << endl; return newM;
+	}
 
 	for (int row = 0; row < sizeRow; row++) {
 		for (int clm = 0; clm < m2.sizeCLM; clm++) {
-			newM.elements[row][clm] = 0;
 			for (int sumCnt = 0; sumCnt < sizeCLM; sumCnt++) {
-				newM.elements[row][clm] += (elements[row][sumCnt] * m2.elements[sumCnt][clm]);
+				newM.elements[row][clm] = 
+					newM.elements[row][clm] + (elements[row][sumCnt] * m2.elements[sumCnt][clm]);
 			}
 		}
 	}
@@ -80,17 +83,37 @@ template<class T> Matrix<T> Matrix<T>::operator*(const Matrix & m2)
 
 template<class T> Matrix<T> Matrix<T>::operator+(const Matrix & m2)
 {
+	Matrix<T> newM = Matrix<T>(sizeRow, m2.sizeCLM);
 	if ((sizeRow != m2.sizeRow) || (sizeCLM != m2.sizeCLM)) {
-		cerr << "matrix size mismatch" << endl;
+		cerr << "matrix size mismatch" << endl; return newM;
 	}
 
-	Matrix<T> newM = Matrix<T>(sizeRow, m2.sizeCLM);
 	for (int row = 0; row < sizeRow; row++) {
 		for (int clm = 0; clm < sizeCLM; clm++) {
 			newM.elements[row][clm] = elements[row][clm] + m2.elements[row][clm];
 		}
 	}
 	return newM;
+}
+
+
+template<class T>  Matrix<T>& Matrix<T>::operator=(const Matrix & m2) {
+	sizeRow = m2.sizeRow;
+	sizeCLM = m2.sizeCLM;
+
+	for (int row = 0; row < sizeRow; row++)
+		delete elements[row];
+	delete elements;
+
+	elements = new T *[sizeRow];
+	for (int row = 0; row < sizeRow; row++)
+		elements[row] = new T[sizeCLM];
+	for (int row = 0; row < sizeRow; row++) {
+		for (int clm = 0; clm < sizeCLM; clm++) {
+			elements[row][clm] = m2.elements[row][clm];
+		}
+	}
+	return *this;
 }
 
 #endif
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,5 +1,8 @@
 #include "GaloisField.h"
 #include "Matrix.h"
+#include <fstream>
+
+using namespace std;
 
 void intializeOutTap(Matrix<GaloisField> * M, int poly);
 void intializeInTap(Matrix<GaloisField> * M, int poly);
@@ -8,18 +11,44 @@ int main(void) {
 	int polyA = 0x000000b1; //1011_0001
 	int polyB = 0x00000080; //1000_0000
 	Matrix<GaloisField> Reg = Matrix<GaloisField>(8, 1);
-	Matrix<GaloisField> TransferM = Matrix<GaloisField>(8, 8);
+	Matrix<GaloisField> TransferOutTap = Matrix<GaloisField>(8, 8);
+	Matrix<GaloisField> TransferInTap = Matrix<GaloisField>(8, 8);
+	ofstream fsOutTap("OutTap.txt");
+	ofstream fsInTap("InTap.txt");
 
 	//intialize
-	intializeOutTap(&TransferM, polyA);
+	intializeOutTap(&TransferOutTap, polyA);
+	Reg.elements[0][0].value = true;
+
+	for (int i = 0; i < (1<<8); i++) {
+		Reg = TransferOutTap * Reg;
+		for (int j = 0; j < Reg.sizeRow; j++) {
+			fsOutTap << ((Reg.elements)[j][0]).value << ' ';
+		}
+		fsOutTap << endl;
+	}
+	fsOutTap.close();
+	
+	//intialize
+	intializeInTap(&TransferInTap, polyA);
+
+	for (int i = 0; i < (1 << 8); i++) {
+		Reg = TransferInTap * Reg;
+		for (int j = 0; j < Reg.sizeRow; j++) {
+			fsInTap << ((Reg.elements)[j][0]).value << ' ';
+		}
+		fsInTap << endl;
+	}
+	fsInTap.close();
 
 	return 0;
 }
 
 //intialize out-tap architecture Transfer Matrix
+//only valid when the elements in Matrix is all false (right after claim)
 void intializeOutTap(Matrix<GaloisField> * M, int poly) {
 	for (int clm = 0; clm < M->sizeCLM; clm++) {
-		M->elements[1][clm] = (poly & (1<< clm)) != 0;
+		M->elements[0][clm] = (poly & (1<< clm)) != 0;
 	}
 	for (int row = 1, clm = 0; row < M->sizeRow; row++, clm++) {
 		M->elements[row][clm] = true;
@@ -27,9 +56,10 @@ void intializeOutTap(Matrix<GaloisField> * M, int poly) {
 }
 
 //intialize out-tap architecture Transfer Matrix
+//only valid when the elements in Matrix is all false (right after claim)
 void intializeInTap(Matrix<GaloisField> * M, int poly) {
 	for (int row = 0; row < M->sizeRow; row++) {
-		M->elements[row][4] = (poly & (1 << (M->sizeRow - row))) != 0;
+		M->elements[row][M->sizeCLM - 1] = (poly & (1 << (M->sizeRow - 1 - row))) != 0;
 	}
 	for (int row = 1, clm = 0; row < M->sizeRow; row++, clm++) {
 		M->elements[row][clm] = true;
